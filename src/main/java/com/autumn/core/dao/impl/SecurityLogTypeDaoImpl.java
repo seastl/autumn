5a0e0e4492914c9c276a07b21f3d76d83ebad7b6
kliang <kliang@eagle.hq.siras.com>
2016-09-28 14:05:08 -0700

    Updated Nn funds.

--- src/main/java/com/autumn/core/dao/impl/SecurityLogTypeDaoImpl.java
+++ src/main/java/com/autumn/core/dao/impl/SecurityLogTypeDaoImpl.java
@@ -248,7 +248,7 @@ public class SecurityLogTypeDaoImpl implements SecurityLogTypeDao {
                                    new LogType(LogType.END_DAY_LOG, "Start of day log", null, null, null, Time.valueOf("13:05:00")),
                                    Boolean.TRUE);
     securities.add(security);
-    security = new SecurityLogType(new Security(1002, "AllianzGI NFJ International Value Instl", "ANJIX", SecurityType.US_MUTUAL_FUND, Sector.UNKNOWN),
+    security = new SecurityLogType(new Security(1002, "Vanguard Treasury Money Market Investor", "VUSXX", SecurityType.US_MUTUAL_FUND, Sector.UNKNOWN),
                                    new LogType(LogType.END_DAY_LOG, "Start of day log", null, null, null, Time.valueOf("13:05:00")),
                                    Boolean.TRUE);
     securities.add(security);
@@ -264,7 +264,7 @@ public class SecurityLogTypeDaoImpl implements SecurityLogTypeDao {
                                    new LogType(LogType.END_DAY_LOG, "Start of day log", null, null, null, Time.valueOf("13:05:00")),
                                    Boolean.TRUE);
     securities.add(security);
-    security = new SecurityLogType(new Security(1006, "American Century Capital Preser", "CPFXX", SecurityType.US_MUTUAL_FUND, Sector.UNKNOWN),
+    security = new SecurityLogType(new Security(1006, "JPMorgan Mid Cap Growth R6", "JMGMX", SecurityType.US_MUTUAL_FUND, Sector.UNKNOWN),
                                    new LogType(LogType.END_DAY_LOG, "Start of day log", null, null, null, Time.valueOf("13:05:00")),
                                    Boolean.TRUE);
     securities.add(security);
@@ -301,10 +301,6 @@ public class SecurityLogTypeDaoImpl implements SecurityLogTypeDao {
                                    Boolean.TRUE);
     securities.add(security);
     security = new SecurityLogType(new Security(1015, "Vanguard Institutional Index I", "VINIX", SecurityType.US_MUTUAL_FUND, Sector.UNKNOWN, true, "22.5%"),
-                                   new LogType(LogType.END_DAY_LOG, "Start of day log", null, null, null, Time.valueOf("13:05:00")),
-                                   Boolean.TRUE);
-    securities.add(security);
-    security = new SecurityLogType(new Security(1016, "AllianzGI NFJ International Value Instl", "ANJIX", SecurityType.US_MUTUAL_FUND, Sector.UNKNOWN, true, "22.5%"),
                                    new LogType(LogType.END_DAY_LOG, "Start of day log", null, null, null, Time.valueOf("13:05:00")),
                                    Boolean.TRUE);
     securities.add(security);
