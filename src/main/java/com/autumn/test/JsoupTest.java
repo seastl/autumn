932ae9f6af021e5bb295793dfc039009204ec170
kliang <kliang@eagle.hq.siras.com>
2018-01-24 17:28:25 -0800

    Added testParseLiveQuote().

--- src/main/java/com/autumn/test/JsoupTest.java
+++ src/main/java/com/autumn/test/JsoupTest.java
@@ -13,7 +13,7 @@ public class JsoupTest {
     String url = "https://finance.yahoo.com/quote/GOOG/history?p=GOOG";
 
     try {
-      //Document doc = Jsoup.connect(url).timeout(60000).maxBodySize(0).get();
+      //Document doc = Jsoup.connect(yUrl).timeout(60000).maxBodySize(0).get();
       Document doc = Jsoup.connect(url)
               .header("Accept-Encoding", "gzip, defalte")
               .userAgent("Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0")
@@ -36,6 +36,36 @@ public class JsoupTest {
       }
 
       System.out.println("*** KL: texts=" + elements);
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+
+    return texts;
+  }
+
+  
+  public String testParseLiveQuote() {
+    String texts = null;
+
+    // Get from yahoo
+    try {
+      String yUrl = "https://finance.yahoo.com/quote/GOOG?p=GOOG";
+      //Document doc = Jsoup.connect(yUrl).timeout(60000).maxBodySize(0).get();
+      Document doc = Jsoup.connect(yUrl)
+              .header("Accept-Encoding", "gzip, defalte")
+              .userAgent("Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0")
+              .timeout(60000)
+              .maxBodySize(0)
+              .get();
+
+      Elements name = doc.getElementsByClass("D(ib) Fz(18px)");
+      System.out.println("*** KL: name=" + name.get(0).text());
+      
+      Elements quotes = doc.getElementsByClass("Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)");
+      System.out.println("*** KL: quote=" + quotes.get(0).text());
+      
+      Elements percent = doc.getElementsByClass("Trsdu(0.3s) Fw(500) Pstart(10px) Fz(24px) C($dataRed)");
+      System.out.println("*** KL: percent=" + percent.get(0).text());
     } catch (Exception ex) {
       ex.printStackTrace();
     }
